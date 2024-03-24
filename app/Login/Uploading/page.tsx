90dbac0cdadfb89c789a0533605ce3296f5acef4
Matthew Hawksby <matthewhawksby91@gmail.com>
2024-03-23 20:55:36 -0700

    add links

---
+++ app/Login/Uploading/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SwipingPage(){
+    return (
+      <main>
+        <div>
+          <p>"Testing!"</p>
+        </div>
+      </main>
+    )
+  }
