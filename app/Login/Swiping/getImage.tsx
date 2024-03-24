65c3a9a0afd2b1dc28a1e20b3ff8099cd443d265
Sam Nygard <samnygard@gmail.com>
2024-03-24 09:59:47 -0700

    random photo

--- app/Login/Swiping/getImage.tsx
+++ app/Login/Swiping/getImage.tsx
@@ -1,12 +1,14 @@
 import { list } from '@vercel/blob';
 
 
+
 export default async function ListAllImages() {
   const response = await list();
     
+    var index = Math.floor(Math.random()*response.blobs.length)
     
     return(
-    <img src={response.blobs[0].downloadUrl} alt={response.blobs[0].pathname} width={500} height={500}/>
+    <img src={response.blobs[index].downloadUrl} alt={response.blobs[index].pathname} width={500} height={500}/>
     )
       {/* {response.blobs.map((blob) => (
         <div key={blob.pathname}>
