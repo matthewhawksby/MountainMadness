4fdda0aba68352e3141f6f330153831140bea643
Matthew Hawksby <matthewhawksby91@gmail.com>
2024-03-24 09:39:51 -0700

    retry

--- app/Login/Uploading/page.tsx
+++ app/Login/Uploading/page.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
-
-export default function SwipingPage(){
-    return (
-      <main>
+'use client';
+ 
+import type { PutBlobResult } from '@vercel/blob';
+import { useState, useRef } from 'react';
+ 
+export default function PetUploadPage() {
+  const inputFileRef = useRef<HTMLInputElement>(null);
+  const [blob, setBlob] = useState<PutBlobResult | null>(null);
+  return (
+    <>
+      <h1>Upload Your Pet Pictures!</h1>
+ 
+      <form
+        onSubmit={async (event) => {
+          event.preventDefault();
+ 
+          if (!inputFileRef.current?.files) {
+            throw new Error('No file selected');
+          }
+ 
+          const file = inputFileRef.current.files[0];
+ 
+          const response = await fetch(
+            `/Login/Uploading?filename=${file.name}`,
+            {
+              method: 'POST',
+              body: file,
+            },
+          );
+ 
+          const newBlob = (await response.json()) as PutBlobResult;
+ 
+          setBlob(newBlob);
+        }}
+      >
+        <input name="file" ref={inputFileRef} type="file" required />
+        <button type="submit">Upload</button>
+      </form>
+      {blob && (
         <div>
-          <p>"Testing!"</p>
+          Blob url: <a href={blob.url}>{blob.url}</a>
         </div>
-      </main>
-    )
-  }
+      )}
+    </>
+  );
+}
---
+++ app/Login/Uploading/route.ts
@@ -0,0 +1,21 @@
+import { put } from '@vercel/blob';
+import { NextResponse } from 'next/server';
+ 
+export async function POST(request: Request): Promise<NextResponse> {
+  const { searchParams } = new URL(request.url);
+  const filename = searchParams.get('filename');
+ 
+ if (filename === null) {
+    throw new Error('Filename not provided');
+  }
+
+  if (request.body === null) {
+    throw new Error('Something is incorrect')
+  }
+
+  const blob = await put(filename, request.body, {
+    access: 'public',
+  });
+ 
+  return NextResponse.json(blob);
+}
